79a8200f001b1afb932d4264434d1e3779957b3b
User <michaelm810129@gmail.com>
2023-03-23 19:02:41 +1030

    sequelize init files/ server template code

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const path = require('path');
-const hbs = require('handlebars');
+const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
 const app = express();
+
 const PORT = process.env.PORT || 3001;
 
+const hbs = exphbs.create({});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname,'public')));
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => {
+  res.render('main');
+})
 
 
-app.listen(PORT, () =>
-  console.log(`Example app listening at http://localhost:${PORT}`)
-);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening at http://localhost:3001'));
+});
