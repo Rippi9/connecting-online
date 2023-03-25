c2cd36eb287455fb81d378307c7b4767e03c6f23
User <michaelm810129@gmail.com>
2023-03-25 20:07:32 +1030

    Added functionality to login button on landing page

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,17 @@
-
+// Landing page login button function
+const loginBtn = document.querySelector("#loginBtn");
+const login = async () => {
+   await fetch("/login")
+    .then((response) =>  {
+      if (response.ok) {
+        document.location.replace('/login'); 
+      } else {
+        console.error("Error: Could not find the page you are looking for!");
+      }
+    })
+    .catch((error) => {
+      console.log("There has been an internal issue");
+    });
+    
+};
+loginBtn.addEventListener("click", login);
