fbc21792da8d494a644bbe4655de6dc2cb85784a
User <michaelm810129@gmail.com>
2023-03-27 21:14:32 +1030

    login formhandler

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,4 @@
 // Landing page login button function
-const loginBtn = document.querySelector("#loginBtn");
 const login = async () => {
    await fetch("/login")
     .then((response) =>  {
@@ -14,4 +13,31 @@ const login = async () => {
     });
     
 };
+const loginBtn = document.querySelector("#loginBtn");
 loginBtn.addEventListener("click", login);
+
+// Login Page handler
+const loginFormHandler = async (event) => {
+  event.preventDefault();
+
+  const email = document.querySelector("#email").value.trim();
+  const password = document.querySelector('#password').value.trim();
+
+  if(email && password) {
+    const response = await fetch('/login', {
+      method: 'POST',
+      body: JSON.stringify({email, password}),
+      headers: { 'Content-Type': 'application/json'},
+    })
+    console.log(`${email}, Pass: ${password}`);
+
+    if(response.ok) {
+      document.location.replace ('/api/dashboard');
+    } else {
+      console.error(error);
+    }
+
+  }
+ };
+ const loginToDashboard = document.querySelector('#sign-up');
+ loginToDashboard.addEventListener("click", loginFormHandler);
