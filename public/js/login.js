ce9499c3b5e739b74230d17557d9052ad5c7cd8f
User <michaelm810129@gmail.com>
2023-03-30 20:41:48 +1030

    added alert with login page sign in/added redirect to signup page

--- public/js/login.js
+++ public/js/login.js
@@ -18,7 +18,7 @@ const loginFormHandler = async (event) => {
     if(response.ok) {
       document.location.replace('/dashboard');
     } else {
-      console.error('Login Failed');
+      alert('Login Failed');
     }
 
   }
